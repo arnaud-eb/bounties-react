232de4f672407387aff1b591bed7211b1c37f002
arnaud-eb <arnaud.depierreux@easebest.com>
2020-11-09 12:58:28 +0100

    Updates

---
+++ precache-manifest.8c731537deffcb50502e8b1a4e933a34.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e96ab28e8e533820e32eaca21eb14773",
+    "url": "/bounties-react/index.html"
+  },
+  {
+    "revision": "85f2dc98289902eecf33",
+    "url": "/bounties-react/static/css/2.afb5e1ff.chunk.css"
+  },
+  {
+    "revision": "ba947320e00f69bd8337",
+    "url": "/bounties-react/static/css/main.39a405ad.chunk.css"
+  },
+  {
+    "revision": "85f2dc98289902eecf33",
+    "url": "/bounties-react/static/js/2.e3b8917b.chunk.js"
+  },
+  {
+    "revision": "ba947320e00f69bd8337",
+    "url": "/bounties-react/static/js/main.e9e18fcd.chunk.js"
+  },
+  {
+    "revision": "2a090a6f96d21067d4e4",
+    "url": "/bounties-react/static/js/runtime-main.1a3bcc82.js"
+  }
+]);
